256752cec50bf63a1af0e9e34adf767fcc947904
jauregi13 <jon.j1305@gmail.com>
2023-07-29 15:55:53 +0200

    Resultado de kilometros a millas y al reves

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,35 +2,42 @@ import './Convert.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRightArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as faRHeart } from '@fortawesome/free-regular-svg-icons';
-import Select from 'react-select';
-import { useEffect, useState } from 'react';
+import { useRef, useState } from 'react';
 
 
 function Convert() {
 
     const [converter,setConverter] = useState({one : 'km',two : 'miles'});
-    const [convertValue,setConvertValue] = useState('0.00');
+    const [convertValue,setConvertValue] = useState(0.00);
+    const selectConversion = useRef();
+    const convertNumber = useRef();
 
     function changeConverter(){
-       var conversionString =  document.getElementById('conversion').value
-       var conversions = conversionString.split('_');
+       var conversions = selectConversion.current.value.split('_');
        setConverter({one : conversions[0], two: conversions[1]});
     }
-
+    /**
+     * Le da la vuelta al convertidor, es decir, por ejemplo en vez de kilometros a millas se convierte de millas a kilometros
+     */
     function reverseConversion(){
-        var conversionString =  document.getElementById('conversion').value
+        var conversionString =  selectConversion.current.value;
         var conversions = conversionString.split('_');
         setConverter({one : conversions[1], two: conversions[0]});
+
+        var aux = convertValue;
+        setConvertValue(convertNumber.current.value);
+        convertNumber.current.value = aux;
+        selectConversion.current.value = conversions[1]+'_'+conversions[0];
     }
 
     function calculateConversion(){
-        switch (document.getElementById('conversion').value) {
+        switch (selectConversion.current.value) {
             case 'km_miles':
-                
+                setConvertValue((convertNumber.current.value / 1.609344).toFixed(2));
                 break;
 
             case 'miles_km':
-
+                setConvertValue((convertNumber.current.value * 1.609344).toFixed(2));
                 break;
         
             case 'feet_metres':
@@ -64,7 +71,7 @@ function Convert() {
                 <div className='content'>
                     <h1>convert</h1>
                     <div className='line_content'>
-                        <select id='conversion' className='converter' onChange={changeConverter}>
+                        <select defaultValue={'km_miles'} id='conversion' ref={selectConversion} className='converter' onChange={changeConverter}>
                             <option value='km_miles'>km - miles</option>
                             <option value='miles_km'>miles - km</option>
                             <option value='feet_metres'>feet - metres</option>
@@ -75,7 +82,7 @@ function Convert() {
                         <FontAwesomeIcon icon={faArrowRightArrowLeft} onClick={reverseConversion}/>
                     </div>
                     <div className='line_content'>
-                        <input type='number'/>
+                        <input type='number' ref={convertNumber} onChange={calculateConversion}/>
                         <label>{converter.one}</label>
                     </div>
                     <div className='line_content result'>
